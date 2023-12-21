d93802b9d44873749371adda3d6b90766d1ecbf3
AIVV <aivv73@gmail.com>
2023-12-22 03:21:08 +0400

    now store car id instead of car name in rentals

--- rental_process.php
+++ rental_process.php
@@ -9,21 +9,40 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_SESSION['username'])) {
     $dropoffLocation = $_POST["dropoffLocation"];
 
     $dropoffDateTime = strtotime($_POST["dropoffDateTime"]);
-    $selectedCar = $_POST["selectedCar"];
-
-    // Calculate the number of days the car will be rented
-    $rentalDays = floor(($dropoffDateTime - $pickupDateTime) / (60 * 60 * 24));
+    $selectedCar = $_POST['selectedCar'];
 
     // Include your database connection code
     include('connection.php');
 
-    // Insert the rental information into the rentals table
-    $insertQuery = "INSERT INTO rentals (username, pickup_location, dropoff_location, pickup_datetime, dropoff_datetime, selected_car, different_dropoff, rental_days)
-                    VALUES ('{$_SESSION['username']}', '$pickupLocation', '$dropoffLocation', FROM_UNIXTIME($pickupDateTime), FROM_UNIXTIME($dropoffDateTime), '$selectedCar', '$differentDropoff','$rentalDays')";
+    // Query to get the car_id based on the selected car_name
+    $getCarIdQuery = "SELECT id FROM cars WHERE car_name = '$selectedCar'";
+    $carIDResult = mysqli_query($con, $getCarIdQuery);
 
-    if (mysqli_query($con, $insertQuery)) {
-        echo "Rental information successfully submitted!";
+    // Check if the query was successful
+    if ($carIDResult) {
+        // Fetch the result as an associative array
+        $row = mysqli_fetch_assoc($carIDResult);
+
+        // Access the 'id' key from the associative array
+        $carID = $row['id'];
+
+        // Calculate the number of days the car will be rented
+        $rentalDays = floor(($dropoffDateTime - $pickupDateTime) / (60 * 60 * 24));
+
+        // Insert the rental information into the rentals table
+        $insertQuery = "INSERT INTO rentals (username, pickup_location, dropoff_location, pickup_datetime, dropoff_datetime, car_id, different_dropoff, rental_days)
+                        VALUES ('{$_SESSION['username']}', '$pickupLocation', '$dropoffLocation', FROM_UNIXTIME($pickupDateTime), FROM_UNIXTIME($dropoffDateTime), $carID, '$differentDropoff', $rentalDays)";
+
+        if (mysqli_query($con, $insertQuery)) {
+            echo "Rental information successfully submitted!";
+        } else {
+            echo "Error: " . mysqli_error($con);
+        }
+
+        // Close the result set
+        mysqli_free_result($carIDResult);
     } else {
+        // Display an error message if the query failed
         echo "Error: " . mysqli_error($con);
     }
 
